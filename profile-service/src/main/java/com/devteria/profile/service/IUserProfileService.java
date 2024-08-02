400651fe5ee971c4ae522dd084a7803652114eb5
Tran Hoang Quan <quan02042004@gmail.com>
2024-08-02 16:36:53 +0700

    #10 - finish config authorize for profile. Mirage check token expire for api gateway

--- profile-service/src/main/java/com/devteria/profile/service/IUserProfileService.java
+++ profile-service/src/main/java/com/devteria/profile/service/IUserProfileService.java
@@ -2,6 +2,7 @@ package com.devteria.profile.service;
 
 import com.devteria.profile.dto.request.ProfileCreationRequest;
 import com.devteria.profile.dto.response.UserProfileResponse;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
@@ -9,5 +10,6 @@ public interface IUserProfileService {
     UserProfileResponse createProfile(ProfileCreationRequest request);
     UserProfileResponse getProfile(String id);
 
+    @PreAuthorize("hasRole('ADMIN')")
     List<UserProfileResponse> getAllProfiles();
 }
