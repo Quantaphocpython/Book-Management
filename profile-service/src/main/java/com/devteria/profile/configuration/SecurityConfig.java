49ae63a7a4bad5ba4522dafeb006453c8346bd81
Tran Hoang Quan <quan02042004@gmail.com>
2024-08-16 23:49:34 +0700

    feat: add security among service

--- profile-service/src/main/java/com/devteria/profile/configuration/SecurityConfig.java
+++ profile-service/src/main/java/com/devteria/profile/configuration/SecurityConfig.java
@@ -16,7 +16,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private static final String[] PUBLIC_ENDPOINTS = {};
+    private static final String[] PUBLIC_ENDPOINTS = {
+            "/internal/users"
+    };
 
     private final CustomJwtDecoder customJwtDecoder;
 
