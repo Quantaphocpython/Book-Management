5d89921a2c84a94220bf04467b155d4e702e7881
Tran Hoang Quan <quan02042004@gmail.com>
2024-08-22 20:45:40 +0700

    feat: config cors

--- api-gateway/src/main/java/com/devteria/api/gateway/config/WebClientConfiguration.java
+++ api-gateway/src/main/java/com/devteria/api/gateway/config/WebClientConfiguration.java
@@ -3,9 +3,14 @@ package com.devteria.api.gateway.config;
 import com.devteria.api.gateway.repository.IdentityClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
 
 @Configuration
 public class WebClientConfiguration {
@@ -14,6 +19,19 @@ public class WebClientConfiguration {
         return WebClient.builder()
                 .baseUrl("http://localhost:8080/identity")
                 .build();
+    }
+
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
+
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
     }
 
     @Bean
