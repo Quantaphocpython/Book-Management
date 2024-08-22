57a6ca3a50b1bec291bd1fecf62ff25439910a9b
Tran Hoang Quan <quan02042004@gmail.com>
2024-08-22 20:47:12 +0700

    #16 -  feat: config cors again again

--- api-gateway/src/main/java/com/devteria/api/gateway/config/WebClientConfiguration.java
+++ api-gateway/src/main/java/com/devteria/api/gateway/config/WebClientConfiguration.java
@@ -32,6 +32,7 @@ public class WebClientConfiguration {
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
 
 
+
         return new CorsWebFilter(urlBasedCorsConfigurationSource);
     }
 
